6ae105d59e5513c958e79184c90328ecf08cd7ed
Juliana <juliana.costa@discover.com.br>
2026-08-16 13:35:16 -0300

    feat: Criação de API Gateway para utilização com lambdas de validação de dados do usuário

---
+++ api-gateway/outputs.tf
@@ -0,0 +1,14 @@
+output "api_gateway_invoke_url" {
+  description = "URL publica do API Gateway"
+  value       = aws_apigatewayv2_stage.default.invoke_url
+}
+
+output "auth_lambda_arn" {
+  description = "ARN da funcao Lambda de emissao do token via CPF/CNPJ, conforme resolvida pelo data source"
+  value       = data.aws_lambda_function.auth.arn
+}
+
+output "auth_authorizer_lambda_arn" {
+  description = "ARN da funcao Lambda Authorizer, conforme resolvida pelo data source"
+  value       = data.aws_lambda_function.authorizer.arn
+}
